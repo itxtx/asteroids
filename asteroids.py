f51b0e9b0927d423e8da299abbeabda24b227c5a
itxtx <68209227+itxtx@users.noreply.github.com>
2024-08-24 23:38:58 +0200

    bugfixes

--- asteroids.py
+++ asteroids.py
@@ -11,11 +11,11 @@ class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
 
-    def update(self, screen):
+    def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, 2)
 
 
-    def draw(self, dt):
+    def update(self, dt):
         self.position += self.velocity * dt
 
 
