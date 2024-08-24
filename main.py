429deceecca7808e0869c0de6a9ae7f8f4deebc7
itxtx <68209227+itxtx@users.noreply.github.com>
2024-08-24 09:43:26 +0200

    add game loop

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-import constants
+from constants import *
 import pygame
 
 
 def main():
     print("Starting asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen  = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+    while True:
+        pygame.Surface.fill(color="black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.quit():
+                return
+
+
+
 
 
 if __name__ == "__main__":
