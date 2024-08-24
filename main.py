cf6c8c05fe63f0fe3a1fdbe33e7b48247f1f2cec
itxtx <68209227+itxtx@users.noreply.github.com>
2024-08-24 09:29:58 +0200

    start game msg

---
+++ main.py
@@ -0,0 +1,22 @@
+import constants
+import pygame
+
+
+def main():
+    print("Starting asteroids!")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
