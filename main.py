0034da29f149cd76fdc90b7789f54db4be648389
itxtx <68209227+itxtx@users.noreply.github.com>
2024-08-24 09:51:11 +0200

    fixes

--- main.py
+++ main.py
@@ -8,10 +8,16 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen  = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    t = pygame.time.Clock()
+    dt = 0
 
     while True:
-        pygame.Surface.fill(color="black")
+        pygame.Surface.fill(screen, color="black")
         pygame.display.flip()
+        
+        t.tick(60)
+
         for event in pygame.event.get():
             if event.type == pygame.quit():
                 return
